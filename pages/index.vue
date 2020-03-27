423223c8befd521dbcb17a3eb3401e7e90a0e4d7
Gerhard Dickescheid <gdd@eacg.de>
2020-03-27 19:40:40 +0100

    Api pi calls with axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: {
+    EventCard
+  },
+  asyncData ({ $axios, error }) {
+    return $axios.get('http://localhost:3000/events')
+      .then(response => (
+        {
+          events: response.data
+        }
+      ))
+      .catch(e =>
+        error({ statusCode: 503, message: 'Unable to fetch events at this time, please try again' })
+      )
+  },
   head () {
     return {
       title: 'Event Listing'
